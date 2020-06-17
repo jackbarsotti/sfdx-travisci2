459b5c469abc66b864f726ecb5269330bc1a8891
Jack Barsotti <jackbarsotti1@gmail.com>
2020-06-17 10:14:19 -0500

    deleted junk files from repo

--- deployuat.sh
+++
@@ -1,4 +0,0 @@
-- echo openssl version
-- sfdx force:org:display -u UAT
-- sfdx force:source:deploy --wait 10 --sourcepath $DEPLOYDIR --testlevel $TESTLEVEL -u UAT
-- sfdx force:apex:test:run -u UAT --wait 10
