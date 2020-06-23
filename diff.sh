db1681c2570e9bf5980b06f63379de83c66b6e87
Jack Barsotti <jackbarsotti1@gmail.com>
2020-06-23 07:53:48 -0500

    test

--- diff.sh
+++ diff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CHANGED_FILES=`git diff --name-only master src/`
+CHANGED_FILES=`git diff --name-only master src`
 
 for CHANGED_FILE in $CHANGED_FILES; do
-  echo ../$CHANGED_FILE
+  echo $CHANGED_FILE
 done
