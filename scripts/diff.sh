e62a07cd856e6c116f4ede03235117340f386ef3
Jack Barsotti <jackbarsotti1@gmail.com>
2020-06-23 20:13:22 -0500

    test

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CHANGED_FILES=`git diff --name-only master src/`
+CHANGED_FILES=`git diff --name-only master force-app/`
 
 for CHANGED_FILE in $CHANGED_FILES; do
   echo ../$CHANGED_FILE
