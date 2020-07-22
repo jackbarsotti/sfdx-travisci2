14d7416630a32d6b233adcaeb2eef199112af144
Jack Barsotti <jackbarsotti1@gmail.com>
2020-07-22 16:07:55 -0500

    test

--- retrieve.sh
+++ retrieve.sh
@@ -54,7 +54,7 @@ echo "Now adding and committing these changes to your $TRAVIS_BRANCH branch..."
 # New
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git add .
+git add force-app/.
 git checkout master
 
 # Git commit -m "auto-build" changes
